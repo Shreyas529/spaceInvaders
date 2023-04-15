2272f0e74e8c2c945351a1d4ec548d9e30255047
malluvkcr7 <122385359+malluvkcr7@users.noreply.github.com>
2023-04-16 01:06:30 +0530

    Add files via upload

--- menu.py
+++ menu.py
@@ -53,6 +53,42 @@ def play():
                     return "NONE"
 
         pygame.display.update()
+def ranking():
+    while True:
+        RANKING_MOUSE_POS=pygame.mouse.get_pos()
+        SCREEN.blit(BG,(0,0))
+        RANKING_EASY = Button(image=None,pos=(640,300),text_input="EASY",font=get_font(75),base_color="Black",hovering_color="Green")
+        RANKING_EASY.changeColor(RANKING_MOUSE_POS)
+        RANKING_EASY.update(SCREEN)
+        RANKING_MEDIUM = Button(image=None,pos=(640,400),text_input="MEDIUM",font=get_font(75),base_color="Black",hovering_color="Green")
+        RANKING_MEDIUM.changeColor(RANKING_MOUSE_POS)
+        RANKING_MEDIUM.update(SCREEN)
+        RANKING_HARD = Button(image=None,pos=(640,500),text_input="HARD",font=get_font(75),base_color="Black",hovering_color="Green")
+        RANKING_HARD.changeColor(RANKING_MOUSE_POS)
+        RANKING_HARD.update(SCREEN)
+        PLAY_BACK = Button(image=None, pos=(640, 600), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        PLAY_BACK.changeColor(RANKING_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if RANKING_EASY.checkForInput(RANKING_MOUSE_POS):
+                    return "EASY"
+                elif RANKING_MEDIUM.checkForInput(RANKING_MOUSE_POS):
+                    return "MEDIUM"
+                elif RANKING_HARD.checkForInput(RANKING_MOUSE_POS):
+                    return "HARD"
+                else:
+                    #main_menu()
+                    return "NONE"
+
+        pygame.display.update()
+
 
 def main_menu():
     value="NONE"
@@ -66,13 +102,16 @@ def main_menu():
 
         PLAY_BUTTON = Button(image=pygame.image.load("Play Rect.png"), pos=(640, 250), 
                             text_input="PLAY", font=get_font(75), base_color="#bf0296", hovering_color="White")
+        RANKING_BUTTON = Button(image=pygame.image.load("Options Rect.png"), pos=(640, 400), 
+                            text_input="RANKING", font=get_font(75), base_color="#bf0296", hovering_color="White")
+        
         
         QUIT_BUTTON = Button(image=pygame.image.load("Quit Rect.png"), pos=(640, 550), 
                             text_input="QUIT", font=get_font(75), base_color="#bf0296", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON,  QUIT_BUTTON]:
+        for button in [PLAY_BUTTON,RANKING_BUTTON,QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
         
@@ -86,6 +125,11 @@ def main_menu():
                     if value!="NONE":
                         return value
                         break
+                if RANKING_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    value= ranking()                    
+                    if value!="NONE":
+                        return value
+                        break    
                
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
